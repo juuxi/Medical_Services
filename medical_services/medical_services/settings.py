97606ca26bff84dd7d2e011b18b224f3d9b7240c
juuxi <dimashopyrev@mail.ru>
2025-12-06 11:46:56 +0300

    startapp services

--- medical_services/medical_services/settings.py
+++ medical_services/medical_services/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core.apps.CoreConfig',
+    'services.apps.ServicesConfig',
     'django_bootstrap5',
 ]
 
