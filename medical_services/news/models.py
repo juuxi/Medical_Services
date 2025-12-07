f7af9dbd724c5e647767f13a75d0b420d2bd4ae1
juuxi <dimashopyrev@mail.ru>
2025-12-07 11:01:27 +0300

    News model

--- medical_services/news/models.py
+++ medical_services/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=64, verbose_name='Заголовок')
+    text = models.TextField(max_length=256, verbose_name='Содержание')
+    is_published = models.BooleanField(verbose_name="Опубликована")
+
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'Новости'
+
+    def __str__(self):
+        return self.title
