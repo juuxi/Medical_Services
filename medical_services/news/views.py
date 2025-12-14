f48ff23a3e3151453ff3234e8b3594f035d5d1dd
juuxi <dimashopyrev@mail.ru>
2025-12-14 21:11:41 +0300

    news impl

--- medical_services/news/views.py
+++ medical_services/news/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import News
 
 
 class NewsListView(ListView):
     model = News
-    ordering = 'id'
+    ordering = 'created_at'
 
 
 class NewsDetailView(DetailView):
     model = News
     pk_url_kwarg = 'news_id'
     template = 'news/news_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_news'] = News.objects.filter(Q(is_published=True) &
+                                                     ~Q(pk=self.object.pk)).order_by('-created_at')[:4]
+        return context
