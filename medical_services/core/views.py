6fe860b39f5ec42041ef6b51a841b89be3bd7511
juuxi <dimashopyrev@mail.ru>
2025-12-14 13:42:50 +0300

    add servies/news to the main page

--- medical_services/core/views.py
+++ medical_services/core/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 
 from django.views.generic import TemplateView
 
+from services.models import Service
+from news.models import News
+
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['services'] = Service.objects.all()[:4]
+        context['news_list'] = News.objects.order_by('id')[:4]
+        return context
+    
 
 class AboutView(TemplateView):
     template_name = 'core/about.html'
