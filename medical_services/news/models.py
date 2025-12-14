4bd11308717b240ee61ca526335164306638cdc2
juuxi <dimashopyrev@mail.ru>
2025-12-14 17:59:44 +0300

    update models

--- medical_services/news/models.py
+++ medical_services/news/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 class News(models.Model):
     title = models.CharField(max_length=64, verbose_name='Заголовок')
+    short_text = models.TextField(max_length=64, 
+                                  verbose_name='Сокращенное содержание')
     text = models.TextField(max_length=256, verbose_name='Содержание')
+    image = models.ImageField('Фото', upload_to='service_images', blank=True)
     is_published = models.BooleanField(verbose_name="Опубликована")
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      verbose_name='Добавлено')
 
 
     class Meta:
--- medical_services/services/models.py
+++ medical_services/services/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 class Service(models.Model):
-    name = models.CharField(max_length=256, verbose_name='Название')
+    name = models.CharField(max_length=64, verbose_name='Название')
+    short_description = models.CharField(max_length=64, verbose_name='Сокращенное описание')
+    description = models.TextField(max_length=256, verbose_name='Описание')
     cost = models.IntegerField(verbose_name="Стоимость")
     duration_minutes = models.SmallIntegerField(verbose_name="Продолжительность")
+    image = models.ImageField('Фото', upload_to='service_images', blank=True)
     is_provided = models.BooleanField(verbose_name="Предоставляется")
+    is_key = models.BooleanField(verbose_name="Ключевая")
 
 
     class Meta:
