5fe3d9aa3b4c562be3de27fa5c54b8afa119d4de
juuxi <dimashopyrev@mail.ru>
2025-12-14 18:22:36 +0300

    image handling

--- medical_services/news/models.py
+++ medical_services/news/models.py
@@ -5,7 +5,7 @@ class News(models.Model):
     short_text = models.TextField(max_length=64, 
                                   verbose_name='Сокращенное содержание')
     text = models.TextField(max_length=256, verbose_name='Содержание')
-    image = models.ImageField('Фото', upload_to='service_images', blank=True)
+    image = models.ImageField('Фото', upload_to='news_images', blank=True)
     is_published = models.BooleanField(verbose_name="Опубликована")
     created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Добавлено')
